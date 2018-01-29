4a070c812741f3705c1d8ae964d64edabe647cec
Adam Binnersley <abinnersley@gmail.com>
2018-01-29 16:00:53 +0000

    Add Unit Tests

---
+++ tests/SitemapTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sitemap\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sitemap\Sitemap;
+
+class SitemapTest extends TestCase{
+    public $sitemap;
+    
+    public function setUp() {
+        parent::setUp();
+    }
+    
+    public function tearDown() {
+        parent::tearDown();
+    }
+    
+    public function testSetDomain() {
+        $this->markTestIncomplete();
+    }
+    
+    public function testGetDomain() {
+        $this->markTestIncomplete();
+    }
+    
+    public function testSetFilePath() {
+        $this->markTestIncomplete();
+    }
+    
+    public function testGetFilePath() {
+        $this->markTestIncomplete();
+    }
+    
+    public function testCreateSitemap() {
+        $this->markTestIncomplete();
+    }
+}
--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @author Adams
- */
-// TODO: check include path
-//ini_set('include_path', ini_get('include_path'));
-
-// put your code here
